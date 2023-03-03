79831f407d7a4a630c56646c7f4a1101fb9dbef2
jacob-armiger <armigersemail@gmail.com>
2023-03-03 16:47:59 -0500

    Add json packets and heartbeat

--- UDPPingerServer.py
+++ UDPPingerServer.py
@@ -2,24 +2,71 @@
 # We will need the following module to generate randomized lost packets
 import random
 from socket import *
+import json
+import time
+import threading
 
-# Create a UDP socket
-# Notice the use of SOCK_DGRAM for UDP packets
-serverSocket = socket(AF_INET, SOCK_DGRAM)
+# {'seq_num': 0, 'timestamp': 16778718072989.03, 'message': 'ping 10.0.0.246'}
+class packetHandler:
+    def __init__(self):
+        self.packet_dict = {'seq_num': 0, 'timestamp': 0, 'message': 'init'}
+        self.last_time = 0
+        self.last_seq = -1
+        self.heartbeat = None
 
-# Assign IP address and port number to socket
-serverSocket.bind(('', 12000))
-print("Server listening on port 12000")
+    def updateHb(self, new_packet):
+        self.packet_dict = new_packet
+        self.last_time = time.time_ns()/1000000
 
-while True:
-    # Generate random number in the range of 0 to 10
-    rand = random.randint(0, 10)
-    # Receive the client packet along with the address it is coming from
-    message, address = serverSocket.recvfrom(1024)
-    # Capitalize the message from the client
-    message = message.upper()
-    # if rand is less than 4, we consider the packet lost and do not respond
-    if rand < 4:
-        continue
-    # otherwise, the server responds
-    serverSocket.sendto(message,address)
+        # Check sequence numbers for missing packet
+        new_seq = new_packet.get('seq_num')
+        if self.last_seq+1 != new_seq and self.last_seq < new_seq:
+            print("Packet missing")
+        self.last_seq = new_packet.get('seq_num')
+    
+    def heartbeatRunner(self):
+        while True:
+            time.sleep(5)
+            time_since_last_packet = (time.time_ns()/1000000) - (self.last_time)
+            if time_since_last_packet > 9000:
+                print("Timeout: no longer recieving packets")
+
+
+
+
+
+
+def main():
+    # Initialize packet handler and heartbeat thread
+    pHandler = packetHandler()
+    pHandler.heartbeart = threading.Thread(target=pHandler.heartbeatRunner, args=(), daemon=True)
+    pHandler.heartbeart.start()
+
+    # Create a UDP socket
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+    # Assign IP address and port number to socket
+    serverSocket.bind(('', 12000))
+    print("Server listening on port 12000")
+
+    while True:
+        # Receive the client packet along with the address it is coming from
+        current_packet, address = serverSocket.recvfrom(1024)
+        current_packet = json.loads(current_packet)
+
+        # Update values for heartbeat calculations
+        pHandler.updateHb(current_packet)
+
+        # Capitalize the message from the client
+        message = current_packet.get('message').upper().encode()
+
+        # Generate random number in the range of 0 to 10
+        rand = random.randint(0, 10)
+        # if rand is less than 4, we consider the packet lost and do not respond
+        if rand < 4:
+            continue
+        # otherwise, the server responds
+        serverSocket.sendto(message,address)
+
+if __name__ == "__main__":
+    main()
--- clientUDP.py
+++ clientUDP.py
@@ -1,5 +1,12 @@
 import socket
 import time
+import json
+
+class Packet:
+    def __init__(self, seq_num, timestamp, message):
+        self.seq_num = seq_num
+        self.timestamp = timestamp / 100000
+        self.message = message
 
 def main():
     rtt_array = []
@@ -8,7 +15,7 @@ def main():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.settimeout(1)
 
-    # Set of server address and port to communicate with
+    # Set up server address and port to communicate with
     server = ("127.0.0.1", 12000)
 
     # Create message to send to server
@@ -16,31 +23,36 @@ def main():
     ping_msg = 'ping ' + address
 
     # Ping the server multiple times
-    pings = 10
-    for i in range(pings):
+    packets = 15
+    for i in range(packets):
+        packet = Packet(i,(time.time_ns()/1000000),ping_msg)
+        packet = json.dumps(packet, default=vars)
+
         # Try until there is a timeout error flagged by the socket
         try:
+
             # Send/recieve message to/from server while tracking time difference for each ping
-            client_socket.sendto(ping_msg.encode(), server)
+            client_socket.sendto(packet.encode(), server)
             start_time = time.time_ns()
             msg_from_server = client_socket.recvfrom(1024)
             end_time = time.time_ns()
 
             # Print message recieved from server with RTT
-            rtt = (end_time - start_time)/100000
-            print(f'{msg_from_server[0].decode()}\n{rtt} ms\n')
+            rtt = (end_time - start_time)/1000000
+            print(f'{msg_from_server[0].decode()} {rtt} ms\n')
 
             # Add rtt to rtt array for later statistics
             rtt_array.append(rtt)
 
         except socket.timeout:
             print("Request timed out\n")
-            
+
+    # Calculate and print statistics
     print(f'Max RTT: {max(rtt_array):>10.2f} ms')
     print(f'Min RTT: {min(rtt_array):>10.2f} ms')
     print(f'Average RTT: {sum(rtt_array)/len(rtt_array):>6.2f} ms')
 
-    packetLossPercent = (1 - (len(rtt_array) / pings)) * 100
+    packetLossPercent = (1 - (len(rtt_array) / packets)) * 100
     print(f'Packet loss:{packetLossPercent:>8.2f}%')
 
 
